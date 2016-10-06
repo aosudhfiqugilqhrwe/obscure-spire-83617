e4f54433a39537f32de95f54576fc97a16eb8a68
aosudhfiqugilqhrwe <aosudhfiqugilqhrwe@gmail.com>
2016-10-06 18:31:21 -0400

    Update main_controller.rb

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,7 @@ class MainController < ApplicationController
   def index
     @hackzors = Hackzor.all
     @name = eval(params[:name].to_s)
-  rescue Exception
+  rescue Exception => e
+    @name = e
   end
 end
